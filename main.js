6e7c9d5b1fb75a3d63ce94e943f7d3f50df51774
Tyler <TylerProbst@Tylers-MacBook-Pro.local>
2015-10-14 13:48:24 -0700

    image onto canvas(still need to size image to canvas)

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// all your js goes here
+$(document).ready(function(){
+	$('form#add').on('change', '#image-upload', function() {
+		var canvas    = document.getElementById('image-workspace');
+		var	context   = canvas.getContext('2d');
+		// var	imgUpload = document.getElementById('image-upload');
+		var img = new Image;
+    	img.src = URL.createObjectURL(event.target.files[0]);
+    	// canvas.height = img.height
+    	// canvas.width = img.width
+
+    	img.onload = function() {
+    		context.drawImage(img, 0, 0, canvas.width, canvas.height);	
+    	}
+		
+	});
+});
