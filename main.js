97f27b9323277ae8ccb7ac66a7710d7b2988ba0e
Tyler <TylerProbst@Tylers-MacBook-Pro.local>
2015-10-14 20:53:45 -0700

    variables hoisted, less generic ID

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-	$('form#add').on('change', '#image-upload', function() {
-		var canvas    = document.getElementById('image-workspace');
-		var	context   = canvas.getContext('2d');
-		// var	imgUpload = document.getElementById('image-upload');
-		var img = new Image;
+	$('form#add-image').on('change', '#image-upload', function() {
+		var canvas    = document.getElementById('image-workspace'),
+			context   = canvas.getContext('2d'),
+		    img 	  = new Image;
+    	
     	img.src = URL.createObjectURL(event.target.files[0]);
 
     	img.onload = function() {
