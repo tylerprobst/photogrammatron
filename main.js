dfdf3ad7574b45e2a48019878536503b880b6fc1
tylerprobst <tyler.probst@ymail.com>
2015-10-14 18:14:04 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 	$('form#add').on('change', '#image-upload', function() {
 		var canvas    = document.getElementById('image-workspace');
 		var	context   = canvas.getContext('2d');
-		// var	imgUpload = document.getElementById('image-upload');
 		var img = new Image;
     	img.src = URL.createObjectURL(event.target.files[0]);
 
