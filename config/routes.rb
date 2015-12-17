8e373f43e413245476559c30ea29ce081c30cee8
Emily Scanlon <emily@Emilys-MacBook-Pro-2.local>
2015-12-16 16:43:01 -0800

    makes privacy policy link work

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   root 'static#index'
 
+  get 'static/privacy_policy'
+
   resources :uploads
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
